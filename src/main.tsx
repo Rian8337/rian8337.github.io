10c508dbd7a56ced64baf61696f2da9400d84d60
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-01-14 18:20:28 +0800

    Switch to `HashRouter` for GitHub pages support

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 
@@ -8,9 +8,9 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("wrapper")!).render(
     <React.StrictMode>
         <HelmetProvider>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </HelmetProvider>
     </React.StrictMode>
 );
